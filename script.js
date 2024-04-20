258939f5446cd982322593e5c8f87e6855e51162
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-20 14:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 //complete this code
-class Person {
+class Person{
 	constructor(name,age){
-		this._name = name;
-		this._age = age;
+		this.name = name;
+		this.age = age;
 	}
 
 	get name(){
-		return this._name;
+		return this.name;
 	}
 
 	set age(age){
-		this._age = age;
+		this.age = age;
 	}
 }
 
-class Student extends Person {
+class Student extends Person{
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this.name} is studying`)
 	}
 }
 
-class Teacher extends Person {
+class Teacher extends Person{
 	teach(){
-		console.log(`${this.name} is teaching`);
+		console.log(`${this.name} is teaching`)
 	}
 }
 
